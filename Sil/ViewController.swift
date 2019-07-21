bf7efa25e94bc72e87e87efcd3f620d2a68a3c6a
Oscar Anzola <oanzola@wayapp.com>
2019-07-21 09:07:22 +0200

    Initial Commit

---
+++ Sil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sil
+//
+//  Created by Oscar Anzola on 7/21/19.
+//  Copyright © 2019 WayApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
