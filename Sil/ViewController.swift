3a7e359212b7ed06077a7aa3fdc28ed043bcfd41
Oscar Anzola <oanzola@wayapp.com>
2020-01-02 15:34:04 +0100

    Initial commit

--- Sil/ViewController.swift
+++ Sil/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var doubleZeroButton: UIButton!
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    lazy var amountFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.usesGroupingSeparator = true
+        formatter.locale = Locale.current
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }()
+
+    var amount: Int = 0 {
+        didSet {
+            amountLabel.text = amountFormatter.string(from: NSNumber(value: Double(amount) / 100.0))
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        amount = 0
     }
-
-
+    
+    @IBAction func doubleZero(_ sender: UIButton) {
+        amount *= 100
+    }
+    
+    @IBAction func deleteButton(_ sender: UIButton) {
+        amount /= 10
+    }
+    
+    @IBAction func numberButton(_ sender: UIButton) {
+        amount = amount*10 + sender.tag
+    }
 }
-
